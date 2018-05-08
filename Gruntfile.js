3cac081d9be2a343e1d87aaea22755808c2f962b
andydevs <akanshul97@gmail.com>
2018-05-07 22:41:30 -0400

    Implement grunt build system

---
+++ Gruntfile.js
@@ -0,0 +1,49 @@
+/**
+ * Serve Karo
+ *
+ * Static web server
+ *
+ * Author:  Anshul Kharbanda
+ * Created: 4 - 24 - 2017
+ */
+
+/**
+ * Configures grunt instance
+ *
+ * @param {Grunt} grunt grunt instance
+ */
+module.exports = function (grunt) {
+    // Initialize config
+    grunt.initConfig({
+        babel: {
+            options: {
+                sourceMap: false,
+                presets: ["@babel/preset-env"]
+            },
+            dist: {
+                files: {
+                    'lib/helper.js': 'src/lib/helper.js',
+                    'lib/servekaro.js': 'src/lib/servekaro.js',
+                    'bin/servekaro': 'src/bin/servekaro.js'
+                }
+            }
+        },
+        mochaTest: {
+            test: {
+                options: {
+                    reporter: 'spec',
+                    require: '@babel/register'
+                },
+                src: ['test/*.js']
+            }
+        }
+    })
+
+    // Load tasks
+    grunt.loadNpmTasks('grunt-babel')
+    grunt.loadNpmTasks('grunt-mocha-test')
+
+    // Register tasks
+    grunt.registerTask('build', ['babel'])
+    grunt.registerTask('test', ['mochaTest'])
+}
