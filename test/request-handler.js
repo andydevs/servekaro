63de40d4f8e9a1d408c13ec30535c66ee9a7934d
andydevs <akanshul97@gmail.com>
2019-05-25 23:35:15 -0400

    Made function and describe stubs for request-handler

--- test/request-handler.js
+++ test/request-handler.js
@@ -7,29 +7,78 @@
  * Created: 4 - 24 - 2017
  */
 import chai, { expect } from 'chai'
+import sinon from 'sinon'
 import chaiHttp from 'chai-http'
+import chaiSinon from 'sinon-chai'
 import chaiHelper from './chai-helper'
 import path from 'path'
 import fs from 'fs'
-import { filepath } from '../lib/request-handler'
-
+import {
+    filepath,
+    buildRequestHandler,
+    sendFile,
+    handleRequest,
+    handleError,
+    handleNotFound,
+    handleNotFoundObject,
+    handleNotFoundString,
+    handleNotFoundDefault
+} from '../lib/request-handler'
 
 // Configure chai
 chai.use(chaiHttp)
 chai.use(chaiHelper)
-
+chai.use(chaiSinon)
 
 // Test filepath functiosn
 describe('filepath', () => {
-    var config = { serving: 'exp', root: '/main.html' }
-
+    var config = { serving: 'exp', root: 'main.html' }
     it('returns a filepath given an http url and config', () => {
         var fp = filepath(config, '/index.html')
         expect(fp).to.equal(path.join('exp', 'index.html'))
     })
-
     it('returns the root filepath if given root url and config', () => {
         var fp = filepath(config, '/')
         expect(fp).to.equal(path.join('exp', 'main.html'))
     })
+})
+
+describe('buildRequestHandler', () => {
+    it('returns handler that calls handleRequest')
+})
+
+describe('sendFile', () => {
+    it('writes file from url')
+    it('writes status if status is given')
+    it('writes root file from config if root url given')
+})
+
+describe('handleError', () => {
+    it('sets status to 500')
+    it('sets content-type to text/plain')
+    it('writes server error text to response')
+})
+
+describe('handleNotFound', () => {
+    it('calls handleNotFoundObject if notFound in config is object')
+    it('calls handleNotFoundString if notFound in config is string')
+    it('calls handleNotFoundDefault if notFound in config is not given')
+})
+
+describe('handleNotFoundObject', () => {
+    it('sets status to status of object')
+    it('sets content type to type of filename')
+    it('writes text from filename of object to response')
+})
+
+describe('handleNotFoundString', () => {
+    it('sets status to 404')
+    it('sets content type to type of filename')
+    it('writes text from given file name to response')
+})
+
+describe('handleNotFoundDefault', () => {
+    it('sets status to 404')
+    it('sets content type to text/plain')
+    it('writes not found text to response')
 })
